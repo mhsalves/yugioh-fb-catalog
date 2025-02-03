34a88131789dcb59046bc9de48b2d646413794bd
Matheus Alves <30807170+mhsalves@users.noreply.github.com>
2025-02-03 17:26:20 -0400

    chore: Simplify cards exporter

--- src/data/cards/index.ts
+++ src/data/cards/index.ts
@@ -1,35 +1,11 @@
-import { ICard } from '../card-types';
-import monsters from './monters';
+import type { ICard } from '../card-types';
+
+import monsters from './monsters';
 import rituals from './rituals';
 import support from './support';
 
-const group0_50: ICard[] = [];
-const group51_100: ICard[] = [];
-const group101_150: ICard[] = [];
-const group151_200: ICard[] = [];
-const group201_250: ICard[] = [];
-const group251_300: ICard[] = [];
-const group301_350: ICard[] = [];
-const group351_400: ICard[] = [];
-const group401_450: ICard[] = [];
-const group451_500: ICard[] = [];
-const group501_550: ICard[] = [];
-const group551_600: ICard[] = [];
-const group601_650: ICard[] = [];
-const group651_700: ICard[] = [];
-const group701_750: ICard[] = [];
-
-const base: ICard[] = [];
-
-export default base.concat(
-    group0_50, group101_150, group51_100,
-    group151_200, group201_250,
-    group251_300, group301_350,
-    group351_400, group401_450,
-    group451_500, group501_550,
-    group551_600, group601_650,
-    group651_700, group701_750,
-    monsters,
-    rituals,
-    support,
+const cards: ICard[] = ([] as ICard[]).concat(
+    monsters, rituals, support,
 ).sort((cardA, cardB) => cardA.id > cardB.id ? 1 : -1);
+
+export default cards;
